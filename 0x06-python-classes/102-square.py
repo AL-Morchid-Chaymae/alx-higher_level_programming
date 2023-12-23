1ca81c753fee28bd5c79f6ce6b3ced324290188b
AL-Morchid-Chaymae <almorchidchaymae1@gmail.com>
2023-12-23 07:51:09 -0800

    task9

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,40 +1,115 @@
 #!/usr/bin/python3
-"""Defines a class MagicClass"""
-import math
+"""Defines a class Square"""
 
 
-class MagicClass:
+class Square:
     """
-    Class that defines properties of MagicClass.
+    Class that defines properties of square by: (based on 3-square.py).
 
     Attributes:
-        radius: radius.
+        size: size of a square (1 side).
     """
-    def __init__(self, radius=0):
-        """Creates new instances of MagicClass.
+    def __init__(self, size=0):
+        """Creates new instances of square.
 
         Args:
-            radius: radius.
-
-        Raises:
-            TypeError: radius must be a number .
+            size: size of the square (1 side).
         """
-        self.__radius = 0
-        if not isinstance(radius, int) and not isinstance(radius, float):
-            raise TypeError("radius must be a number")
-        else:
-            self.__radius = radius
+        self.__size = size
 
     def area(self):
-        """Returns area
+        """Calculates the area of square.
 
-        Returns: area.
+        Returns: the current square area.
         """
-        return math.pi * self.__radius ** 2
+        return self.__size ** 2
 
-    def circumference(self):
-        """Returns circumference
-
-        Returns: circumference.
+    @property
+    def size(self):
+        """Returns the size of a square
         """
-        return 2 * math.pi * self.__radius
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def __lt__(self, other):
+        """Rich comparison operator to compare if square area is less
+        than another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false.
+        """
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        """Rich comparison operator to compare if square area is less
+        than or equal to another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false.
+        """
+        return self.__size <= other.__size
+
+    def __eq__(self, other):
+        """Rich comparison operator to compare if square area is equal to
+        another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size == other.__size
+
+    def __ne__(self, other):
+        """Rich comparison operator to compare if square area is not
+        equal to another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        """Rich comparison operator to compare if square area is greater
+        than another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        """Rich comparison operator to compare if square area is greater
+        than or equal to another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size >= other.__size
