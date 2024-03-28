1dd0a03d19f1fc75a0132591c411661a6f7da4c1
AL-Morchid-Chaymae <almorchidchaymae1@gmail.com>
2024-03-28 13:03:15 -0700

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,3 +1,15 @@
-#!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI $1 | grep "Content-Length" | cut -d " " -f2
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    """fetches https://intranet.hbtn.io/status"""
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        html_str = html.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html_str))
